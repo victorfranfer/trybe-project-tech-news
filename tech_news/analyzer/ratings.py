52642892be225a632947d119e494bd4c1c43c9fa
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-03-13 20:32:31 -0300

    req10:top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,14 +1,16 @@
-from tech_news.database import get_collection
+from tech_news.database import find_news
 from collections import Counter
 
 
 # Requisito 10
 def top_5_categories():
     news = []
-    for article in get_collection():
+    for article in find_news():
         news.append(article["category"])
 
-    categories_counter = Counter(news)
-    sorted_categories_list = sorted(categories_counter.items(),
-                                    key=lambda x: x[1], reverse=True)
-    return sorted_categories_list
+    categories_counter = Counter(news).most_common(5)
+    sorted_categories_list = sorted(categories_counter,
+                                    key=lambda x: (-x[1], x[0]))
+    slice_categories = [tuple[0] for tuple in sorted_categories_list]
+
+    return slice_categories
