20036743ddfc4048e8e85d589f58abb3ffc61b9a
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-03-13 20:05:28 -0300

    req8:search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -12,7 +13,16 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        date_object = datetime.strptime(date, "%Y-%m-%d")
+        new_format_date = date_object.strftime("%d/%m/%Y")
+        query = {"timestamp": {"$regex": new_format_date, "$options": "i"}}
+        news = []
+        for article in search_news(query):
+            news.append((article["title"], article["url"]))
+        return news
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
