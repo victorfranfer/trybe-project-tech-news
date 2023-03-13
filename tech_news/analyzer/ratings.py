600782b15a148a5e24649b12369e5f79bce27460
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-03-13 20:18:40 -0300

    req9:search_by_category

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,14 @@
+from tech_news.database import get_collection
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = []
+    for article in get_collection():
+        news.append(article["category"])
+
+    categories_counter = Counter(news)
+    sorted_categories_list = sorted(categories_counter.items(),
+                                    key=lambda x: x[1], reverse=True)
+    return sorted_categories_list
