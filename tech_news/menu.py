6fc438d180aed2277df4672c64451569a36dce07
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-03-14 14:23:44 -0300

    req12:flake8_fix

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -8,6 +8,17 @@ from tech_news.analyzer.ratings import top_5_categories
 
 # Requisitos 11 e 12
 def analyzer_menu():
+    INPUT_MAPPING = {
+        "0": lambda: get_tech_news(int(input(
+            "Digite quantas notícias serão buscadas:"))),
+        "1": lambda: search_by_title(input("Digite o título:")),
+        "2": lambda: search_by_date(input(
+            "Digite a data no formato aaaa-mm-dd:")),
+        "3": lambda: search_by_category(input("Digite a categoria:")),
+        "4": top_5_categories,
+        "5": lambda: (print("Encerrando script\n")),
+    }
+
     user_input = input("""Selecione uma das opções a seguir:
  0 - Popular o banco com notícias;
  1 - Buscar notícias por título;
@@ -15,25 +26,7 @@ def analyzer_menu():
  3 - Buscar notícias por categoria;
  4 - Listar top 5 categorias;
  5 - Sair. """)
-    if user_input == "0":
-        print("Digite quantas notícias serão buscadas:")
-        amount = input()
-        get_tech_news(int(amount))
-    elif user_input == "1":
-        print("Digite o título:")
-        title = input()
-        search_by_title(title)
-    elif user_input == "2":
-        print("Digite a data no formato aaaa-mm-dd:")
-        date = input()
-        search_by_date(date)
-    elif user_input == "3":
-        print("Digite a categoria:")
-        category = input()
-        search_by_category(category)
-    elif user_input == "4":
-        top_5_categories()
-    elif user_input == "5":
-        print("Encerrando script\n")
+    if user_input in INPUT_MAPPING:
+        INPUT_MAPPING[user_input]()
     else:
         sys.stderr.write("Opção inválida\n")
